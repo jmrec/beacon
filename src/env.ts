241fdd5f01795823fe1ade8b757893ebf22f04c6
Juan Miguel Recondo <134484809+jmrec@users.noreply.github.com>
2026-08-31 19:32:41 +0000

    feat(t3-env): create env environment schema

--- src/env.ts
+++ src/env.ts
@@ -3,37 +3,21 @@ import { z } from "zod";
 
 export const env = createEnv({
   server: {
-    SERVER_URL: z.string().url().optional(),
+    SERVER_URL: z.url().optional(),
+    BENECO_UNSCHEDULED_OUTAGE_URL: z.url().optional(),
+    BENECO_SCHEDULED_OUTAGE_URL: z.url().optional(),
   },
 
-  /**
-   * The prefix that client-side variables must have. This is enforced both at
-   * a type-level and at runtime.
-   */
   clientPrefix: "VITE_",
 
   client: {
     VITE_APP_TITLE: z.string().min(1).optional(),
+    VITE_PH_BOUNDARIES_URL: z
+      .url()
+      .default("https://ph-boundaries.jmrecondo.com/benguet"),
   },
 
-  /**
-   * What object holds the environment variables at runtime. This is usually
-   * `process.env` or `import.meta.env`.
-   */
   runtimeEnv: import.meta.env,
 
-  /**
-   * By default, this library will feed the environment variables directly to
-   * the Zod validator.
-   *
-   * This means that if you have an empty string for a value that is supposed
-   * to be a number (e.g. `PORT=` in a ".env" file), Zod will incorrectly flag
-   * it as a type mismatch violation. Additionally, if you have an empty string
-   * for a value that is supposed to be a string with a default value (e.g.
-   * `DOMAIN=` in an ".env" file), the default value will never be applied.
-   *
-   * In order to solve these issues, we recommend that all new projects
-   * explicitly specify this option as true.
-   */
   emptyStringAsUndefined: true,
 });
